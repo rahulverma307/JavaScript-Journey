3f9b130e7a9600949812df2eb7e52871c013559d
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2026-02-24 07:22:43 +0000

    practice

--- 4.Array/arrMethodprac.js
+++ 4.Array/arrMethodprac.js
@@ -16,3 +16,35 @@ addName.forEach(naam=>console.log("Mr.",naam))
 //Map
 //Q1
 let num=[1,2,3];
+let doubled=num.map(n=>n*2)
+console.log(doubled);
+//Q2
+let num1=[10,20,30]
+let added=num1.map(add=>add+5)
+console.log(added);
+//Q3
+let naam=["rahul","amit","neha"]
+const capital=naam.map(upper=>upper.toUpperCase())
+console.log(capital);
+//Q4 Convert number to object
+let num2=[1,2,3]
+let objj=num2.map(obj=>({obj}));
+console.log(objj);
+
+//Q5 
+let value=[3,5,7];
+let square=value.map(val=>val*val)
+console.log(square);
+
+//Q6 Get string length
+
+let char=["js","react","node"];
+let lengthofString=char.map(len=>len.length)
+console.log(lengthofString);
+
+//
+
+
+
+
+
