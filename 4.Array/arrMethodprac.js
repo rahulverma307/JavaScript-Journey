01b41af224bd23331cba83eab4a5b48f677d4c2d
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2026-02-24 04:44:44 +0000

    prac

---
+++ 4.Array/arrMethodprac.js
@@ -0,0 +1,18 @@
+// for each
+
+//Print all Number
+
+let number=[44,98,45,12,45,6,8,2,6,4,4,55,99,6,2]
+number.forEach(num=>console.log(num))
+
+// Given [10,20,30], print square of each number.
+let squareOfNUmber=[10,20,30]
+squareOfNUmber.forEach(num=>console.log(num*num))
+
+//Add "Mr." before every name in ["Rahul","Amit"].
+let addName=["Rahul","Amit"];
+addName.forEach(naam=>console.log("Mr.",naam))
+
+//Map
+//Q1
+let num=[1,2,3];
