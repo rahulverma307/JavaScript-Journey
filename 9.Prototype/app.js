01febc774770951ec27d2946726cb54e2075a214
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2026-02-27 15:23:46 +0000

    Prototype

---
+++ 9.Prototype/app.js
@@ -0,0 +1,42 @@
+// Jai shree Ram
+//Prototype
+
+const prithviraj={
+    name:"Prithviraj",
+    generation:"grandfather",
+    cookTraditionalDish(){
+        return ` ${this.name} cook an ancient family recipe `
+    }
+};
+// no. one use of protoype is inheritance
+
+const raj=Object.create(prithviraj)
+//console.log(raj);//{}
+//console.log(raj.name);//Prithviraj
+raj.name="raj"
+raj.generation="father"
+raj.runBusiness=function (){
+    return `${this.name} runs the family business`
+}
+console.log(raj);/**{
+  name: 'raj',
+  generation: 'father',
+  runBusiness: [Function (anonymous)]
+} */
+
+  const ranbir=Object.create(raj)
+  ranbir.name="ranbir"
+  ranbir.generation="son"
+  ranbir.makefilms=function(){
+    return `${this.name} directs blockbuster movies`
+  }
+
+  console.log(ranbir.makefilms());
+  console.log(ranbir.runBusiness());
+  console.log(ranbir.cookTraditionalDish());
+
+
+
+
+
+
