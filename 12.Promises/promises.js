438799687ed9b3a6d9eee8f03402640e252d3c5c
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2026-02-28 06:46:51 +0000

    promise start

--- 12.Promises/promises.js
+++ 12.Promises/promises.js
@@ -1,1 +1,61 @@
 /////Jai Shree Ram/////
+
+// we dont use this syntax//
+
+// function prepareOrderCB(dish,cb){
+//     setTimeout(() => cb(null,{dish,status:"prepared"}),1000)
+// }
+
+// function pickupOrderCB(order,cb){
+//     setTimeout(() => cb(null,{...order,status:"picked up"}),1000)
+// }
+
+// function deliverOrder(order,cb){
+//     setTimeout(() => cb(null,{...order,status:"delivered"}),1000)
+// }
+
+// prepareOrderCB("biryani",(err,order)=>{
+//     if(err) return console.log(err);
+//     pickupOrderCB(order,(err,order)=>{
+//         if(err) return console.log(err)
+//     })
+// })
+
+///  pending ,fulfilled, rejected
+
+function prepareOrder(dish){
+    return new Promise((resolve,reject)=>{
+        if(!dish){
+            reject(new Error("No dish is there"))
+            return 
+        }
+        console.log(`${dish} is ready`);
+        resolve({dish,status:"prepared"})   
+    },1000)
+}
+
+
+function pickupOrder(order){
+    return new Promise((resolve,reject)=>{
+        
+        console.log(`${order} is ready`);
+        resolve({dish,status:"pickuped"})   
+    },1000)
+}
+
+function deliverOrder(order){
+    return new Promise((resolve,reject)=>{
+        
+        console.log(`${order} is ready`);
+        resolve({dish,status:"delivered"})   
+    },1000)
+}
+
+
+prepareOrder("Chai")
+ .then(order=>pickupOrder(order))
+ .then(order=>deliverOrder(order))
+ .catch()
+
+
+
