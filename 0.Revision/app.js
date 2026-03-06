7a20e47bc0aadcf33029fd91d4e25e64dd36b6da
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2026-03-06 11:37:04 +0000

    revision

---
+++ 0.Revision/app.js
@@ -0,0 +1,15 @@
+/// sort a number
+
+let number=[4545,7,8,41,5,48,2,4]
+// console.log(number.sort((a,b)=>a-b));//ascending
+// console.log(number.sort((a,b)=>b-a));//desc
+
+//arange name with alphabet
+
+const names=["Atchuyta","Guaranga","Gopal","Kubera","Krsna","Madhav","Radharaman"]
+// console.log(names.sort((a,b)=>a.localeCompare(b)));
+
+
+
+
+
