6abdfc6e87a12125019e8d9ec9eba7fc678e4206
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2026-03-10 05:48:03 +0000

    constructor

--- 15.OOP/app.js
+++ 15.OOP/app.js
@@ -1,10 +1,18 @@
-function Pencil(name,color,price,compnayName){
+function Pencil(name,color,price,compnayName,feedback){
     this.name=name;
     this.color=color;
     this.price=price;
     this.compnayName=compnayName;
     
-    VideoPlaybackQuality()
+    this.quality=function (){
+         console.log(feedback);
+         
+    }
 
 
 }
+let pencil1=new Pencil("natraj","red",10,"ITC","4")
+console.log(pencil1.quality());
+let pencil2=new Pencil("absara","yellow",20,"Doms",8)
+console.log(pencil2.quality());
+
