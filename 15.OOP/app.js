0be2219200b6ccd9ac0896534b21b31075bcdd15
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2026-03-10 05:44:07 +0000

    function

---
+++ 15.OOP/app.js
@@ -0,0 +1,10 @@
+function Pencil(name,color,price,compnayName){
+    this.name=name;
+    this.color=color;
+    this.price=price;
+    this.compnayName=compnayName;
+    
+    VideoPlaybackQuality()
+
+
+}
